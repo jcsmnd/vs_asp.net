11537e7849e601d555c4f650c2d6a12330a6c07f
Administrator <jcsmnd@gmail.com>
2019-04-01 00:06:06 -0400

    add MVC portpolio

---
+++ ASP.NET_WebAPI/Models/sampleModel.cs
@@ -0,0 +1,18 @@
+﻿//Written by Myungsik Kim
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace asp.net_webapi.Models
+{
+    public class sampleModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Comment { get; set; }
+    }
+}
