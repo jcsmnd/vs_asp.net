6878a0962c9a8ccd0b2fcae51a3110d39c7c1673
Administrator <jcsmnd@gmail.com>
2019-04-01 02:00:06 -0400

    get post put delete

--- asp.net webapi/Controllers/SampleController.cs
+++ asp.net webapi/Controllers/SampleController.cs
@@ -12,16 +12,39 @@ namespace asp.net_webapi.Controllers
 {
     public class SampleController : ApiController
     {
-        sampleModel[] model01 = new sampleModel[] {
-            new sampleModel { Id = 1, FirstName = "Myungsik", LastName = "Kim", Comment = "test controller" }
-        };
+        /*
+        List<sampleModel> model01 = new List<sampleModel>();
 
-        public IEnumerable<sampleModel> GetAllProducts()
+        public SampleController()
+        {
+            model01.Add(new sampleModel {Id =1, FirstName="Myungsik", LastName="Kim", Comment="test"});
+        }
+
+        public List<sampleModel> GetAllProducts()
         {
             return model01;
         }
 
-        public IHttpActionResult GetProduct(int id)
+        public sampleModel Get(int id)
+        {
+            return model01.Where(x => x.Id == id).FirstOrDefault();
+        }
+        */
+
+        sampleModel[] model01 = new sampleModel[]
+ {
+            new sampleModel { Id = 1, FirstName = "Myungsik", LastName = "Kim", Comment = "test" },
+            new sampleModel { Id = 2, FirstName = "Hub", LastName = "Git", Comment = "test2"}
+ };
+
+        // GET api/sample
+        public IEnumerable<sampleModel> Get()
+        {
+            return model01;
+        }
+
+        // GET api/sample/1
+        public IHttpActionResult Get(int id)
         {
             var ID = model01.FirstOrDefault((p) => p.Id == id);
             if (ID == null)
@@ -29,6 +52,21 @@ namespace asp.net_webapi.Controllers
                 return NotFound();
             }
             return Ok(ID);
+        }
+
+        // POST api/sample
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/sample/1
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/sample/1
+        public void Delete(int id)
+        {
         }
     }
 }
